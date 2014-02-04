4066e2b42b39a5192bc7ea3db772e7eb2920ed08
Andrew Mleczko <andrew@mleczko.net>
2014-02-04 18:38:01 +0100

    Preparing release 2.1.18

--- setup.py
+++ setup.py
@@ -90,7 +90,7 @@ tests_require = [
 
 
 setup(name='penelope.core',
-      version='2.1.18.dev0',
+      version='2.1.18',
       description='Penelope main package',
       long_description=README + '\n\n' +  CHANGES,
       classifiers=[
