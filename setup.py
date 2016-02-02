44188973faa1f4a4e62c44ddaf252187564ce49f
Andrew Mleczko <andrew@mleczko.net>
2016-02-02 12:43:24 +0100

    Back to development: 2.1.42

--- setup.py
+++ setup.py
@@ -90,7 +90,7 @@ tests_require = [
 
 
 setup(name='penelope.core',
-      version='2.1.41',
+      version='2.1.42.dev0',
       description='Penelope main package',
       long_description=README + '\n\n' +  CHANGES,
       classifiers=[
